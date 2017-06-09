fefe7ec060cebf0ad2db01f9a423b793f942ea86
nhuyp <ypham87@gmail.com>
2017-06-09 16:46:23 -0700

    Allow user to continue purchase or not

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,9 +16,6 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-
-
-
 });
 
 // This application will first display all of the items available for sale, including IDs, names, and prices of products for sale. 
@@ -26,8 +23,10 @@ connection.connect(function (err) {
 function displayInventory() {
     var query = "Select * FROM products";
     var Table = require('cli-table');
+    // Query to display table. 
     connection.query(query, function (err, res) {
         if (err) throw err;
+
         // Add values to table using CLI npm package
         // Instantiate 
         var table = new Table({
@@ -38,62 +37,83 @@ function displayInventory() {
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
         }
-
-        console.log(table.toString());
+        console.log(table.toString());        
+        // Inquirer to prompt user. 
+        promptUser();
     });
+
+
 }; // Closes displayInventory. 
 
-displayInventory();
 
 // Prompt the user and ask what is the id of the product they would like to buy. 
 var promptUser = function () {
-    inquirer.prompt({
+    inquirer.prompt([{
         name: "action",
         type: "input",
         message: "What is the id of the product you would like to buy?",
 
-    }).then(function (user) {
-        console.log(user.action);
-        inquirer.prompt({
-            name: "quantity",
-            type: "input",
-            message: "What is the quantity you would like to purchase? ",
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "What is the quantity you would like to purchase? ",
 
-        }).then(function (user) {
-            console.log(user.quantity);
-            checkQuantity();
-        });
+    }]).then(function (user) {
+
+        checkQuantity(user);
+
     });
 
 }; // Closes promptUser function. 
-
-
 
 // Check if your store has enough of the product to meet the customer's request. 
 // If not, display "insufficient quantity" and prevent the order from going through. 
 // If it does, fulfill the user's order. User should be shown the total cost of their purchase and update should reflect in SQL database. 
 
 // Function to check product's stock quantity. 
-var checkQuantity = function () {
-    var query = "SELECT stock_quantity FROM products where item_id = ?";
-    connection.query(query, [user.action], function (err, res) {
-        if (res[0].stock_quantity > user.quantity ) {
-            purchaseQuantity(); 
+var checkQuantity = function (user) {
+    connection.query("SELECT * FROM products WHERE item_id = ?", [user.action], function (err, res) {
+        if (res[0].stock_quantity > user.quantity) {
+            purchaseQuantity(user, res);
         } else {
-            console.log ("Insufficient quantities available!"); 
+            console.log("Insufficient quantities available!");
+            contPurchase(); 
         }
     });
 };
 
 
 // Function to update product's stock quantity after user "purchase". 
-var purchaseQuantity = function () {
+var purchaseQuantity = function (user, res) {
     var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ? ";
-    connection.query(query, [user.quantity, user.action], function (err, res) {
-        console.log(res); 
+    connection.query(query, [res[0].stock_quantity-parseInt(user.quantity), user.action], function (err, resUpdate) {
+       console.log("You have completed your purchase."); 
     });
+    
+   contPurchase();  
 };
 
-promptUser(); 
+// Function to allow user to make another purchase. Also restarts app. 
+var contPurchase = function () {
+    // Ask user if he or she would like to continue purchases. 
+    inquirer.prompt([{
+        name: "action",
+        type: "list",
+        message: "Would you like to make another purchase?",
+        choices: ["Yes", "No"]
 
-connection.end(); 
+    }]).then(function (user) {
+        if (user.action == "Yes") {
+            displayInventory(); 
+        } else {
+            console.log("Thank you. Have a nice day.");
+            connection.end(); 
+        }
+    
+    });
+
+}; 
+
+
+displayInventory();
+
