f9d95d3c359ae1014b7d03893be1798f1b637718
nhuyp <ypham87@gmail.com>
2017-06-11 21:40:42 -0700

    bamazonManager view product and view low inventory functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,10 +17,74 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 });
+ 
 
-// This application will first display all of the items available for sale, including IDs, names, and prices of products for sale. 
 
-function displayInventory() {
+
+
+// List of menu options:
+// - view products for sale 
+//     - list every available item (item id, names, prices, and quantities)
+// - view low inventory 
+//     - list all items with inventory count lower than 5
+// - add to inventory 
+//     - display prompt that will let the manager add more of any item currently in the store 
+// - add new product 
+//     - allow manager to add a completely new product to the store 
+
+// Main menu list. 
+var mainMenu = function () {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View products for sale",
+            "View low inventory",
+            "Add to inventory",
+            "Add new product"
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case "View products for sale":
+                viewProduct();
+                break;
+            case "View low inventory":
+                lowInventory();
+                break;
+            case "Add to inventory":
+                addInventory();
+                break;
+            case "Add new product":
+                addProduct();
+                break;
+        }
+    });
+};
+
+// Function to allow user to make another purchase. Also restarts app. 
+var contPurchase = function () {
+    // Ask user if he or she would like to continue purchases. 
+    inquirer.prompt([{
+        name: "action",
+        type: "list",
+        message: "Would you like to return to the main menu?",
+        choices: ["Yes", "No"]
+
+    }]).then(function (user) {
+        if (user.action == "Yes") {
+            mainMenu(); 
+        } else {
+            console.log("Thank you. Have a nice day.");
+            connection.end(); 
+        }
+    
+    });
+
+}; 
+
+// Function to view products for sale and list ids, product names, quantities. 
+function viewProduct() {
     var query = "Select * FROM products";
     var Table = require('cli-table');
     // Query to display table. 
@@ -37,50 +101,39 @@ function displayInventory() {
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
         }
-        console.log(table.toString());        
+        console.log(table.toString());
         // Inquirer to prompt user. 
-        promptUser();
+        contPurchase();
     });
+}; // Closes viewProduct function.
 
 
-}; // Closes displayInventory.
+// Function to view low inventory where quantities are lower than 5. 
+
+function lowInventory() {
+    var query = "Select * FROM products WHERE stock_quantity < 5";
+    var Table = require('cli-table');
+    // Query to display table. 
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        // Add values to table using CLI npm package
+        // Instantiate 
+        var table = new Table({
+            head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"], colWidths: [10, 20, 20, 20, 20]
+        });
+
+        // Loop through SQL table and push array into table in console.
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        // Inquirer to prompt user. 
+        contPurchase();
+    });
+}; // Closes viewProduct function.
+
+// Function to allow manager to add quantity to any product in the store. 
 
 
-// List of menu options:
-// - view products for sale 
-//     - list every available item (item id, names, prices, and quantities)
-// - view low inventory 
-//     - list all items with inventory count lower than 5
-// - add to inventory 
-//     - display prompt that will let the manager add more of any item currently in the store 
-// - add new product 
-//     - allow manager to add a completely new product to the store 
-
-// Main menu list. 
-inquirer.prompt({
-    name: "action",
-    type: "rawlist",
-    message: "What would you like to do?",
-    choices: [
-      "View products for sale",
-      "View low inventory",
-      "Add to inventory",
-      "Add new product"
-    ]
-  }).then(function(answer) {
-    switch (answer.action) {
-      case "View products for sale":
-        artistSearch();
-        break;
-      case "View low inventory":
-        multiSearch();
-        break;
-      case "Add to inventory":
-        rangeSearch();
-        break;
-      case "Add new product":
-        songSearch();
-        break;
-    }
-  });
-};
+mainMenu();
